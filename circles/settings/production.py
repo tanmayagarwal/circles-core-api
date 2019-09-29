983d2a38bf3d7cfae3e67031d49aa51743a25fb2
Peter Odeny <odenypeter@gmail.com>
2019-09-29 13:59:38 +0300

    add local sample file

--- circles/settings/production.py
+++ circles/settings/production.py
@@ -5,11 +5,15 @@ from .base import *
 DEBUG = False
 
 
-# Use nose to run all tests
-TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+'''DATABASE CONFIGURATION'''
+DATABASES = {
+    'default': {
+        'ENGINE': environ.get('CIRCLES_DB_ENGINE', ''),
+        'NAME': environ.get('CIRCLES_DB_NAME', ''),
+        'USER': environ.get('CIRCLES_DB_USER', ''),
+        'PASSWORD': environ.get('CIRCLES_DB_PASSWORD', ''),
+        'HOST': environ.get('CIRCLES_DB_HOST', ''),
+        'PORT': environ.get('CIRCLES_DB_PORT', ''),
+    }
+}
 
-# Tell nose to measure coverage on the 'foo' and 'bar' apps
-NOSE_ARGS = [
-    '--with-coverage',
-    '--cover-package=foo,bar',
-]
