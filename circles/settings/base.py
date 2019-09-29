75abd6cd90bba776d8a452103849013e446a8c49
Peter Odeny <odenypeter@gmail.com>
2019-09-29 14:58:45 +0300

    change app root

--- circles/settings/base.py
+++ circles/settings/base.py
@@ -15,7 +15,7 @@ import os
 from datetime import timedelta
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
 # Quick-start development settings - unsuitable for production
@@ -145,6 +145,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+print('Base Directory::::', BASE_DIR)
 
 LOGIN_URL = '/api/v1/api-auth/login'
 LOGOUT_URL = 'api/v1/api-auth/logout'
